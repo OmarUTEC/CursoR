8647e6c7843c5d66ef59236c217639f40b1f2af3
Om@r <javier.chavarria@utec.edu.pe>
2023-01-16 16:13:58 -0500

    add information

--- CrearVariables.R
+++ CrearVariables.R
@@ -1,4 +1,7 @@
-
-# Asignar un valor a una variable:
+# Asignar un valor a una variable y mostrarlo en consola:
 
 x <- 5
+x
+
+# Asimismo se puede usar el operador (=), pero 
+# lo recomendable es utilizar (<-) para asignar un valor a una determinada variabla :)
